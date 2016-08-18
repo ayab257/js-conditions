4edd87edb872635b1061cbbab54852d4680e494b
ayab257 <ayab257@gmail.com>
2016-08-18 09:52:08 +0300

    first

---
+++ script.js
@@ -0,0 +1,84 @@
+
+//EX.1.1
+
+// var one= 3;
+// var two= 1;
+// if (one > two){
+	// console.log(one);
+// }
+// else{
+	// console.log(two);
+// }
+
+// EX.1.2
+// 
+// var x=3, y=-7, z=2;
+// if ((x+y+z) >= 0){
+	// console.log("The sign is + ");
+// }
+// else{
+	// console.log("The sign is -");
+// }
+
+//EX.1.3
+// var x=0, y=-1,z=4;
+// var num =[];
+// if ((x>y) && (x>z) && (z>y)){
+	// num[0]= x;
+	// num[1]=z;
+	// num[2]=y;
+// }
+// 
+// else if ((y>z) && (y>x) && (x>z) ){
+	// num[0] =y;
+	// num[1]=x;
+	// num[2]=z;
+// }
+// else if ((z>y) && (z>x) && (x>y)){
+	// num[0] =z;
+	// num[1]=x;
+	// num[2]=y;
+// }
+// console.log(num);
+
+//EX.1.4
+// var a=-5, b=-2,c=-6,d=0,e=-1;
+// if ((a>b)&& (a>c) && (a>d) && (a>e)){
+	// console.log(a + " is the biggest number");
+// }
+// else if((b>a)&& (b>c) && (b>d) && (b>e)){
+	// console.log(b + " is the biggest number");
+// }
+// else if((c>a) && (c>b) && (c>d) && (c>e)){
+	// console.log(c + " is the biggest number");
+// }
+// else if((d>a) && (d>b) && (d>c) && (d>e)){
+	// console.log(d + " is the biggest number");
+// }
+// else if((e>a) && (e>b) && (e>c) && (e>d)){
+	// console.log(e + " is the biggest number");
+// }
+
+
+//EX.1.5
+// var a=80, b=77, c=88, d=95, e=68;
+// var score =(a+b+c+d+e)/5;
+// console.log(score);
+// if (score < 60){
+	// console.log("גרוע מאוד");
+// }
+// if (60 < score < 70){
+	// console.log("גרוע");
+// }
+// if  (70 < score < 80){
+	// console.log("בינוני");
+// }
+// if  (80 < score < 90){
+	// console.log("טוב");
+// }
+// if (90 < score <= 100){
+	// console.log("טוב מאוד");
+// }
+
+var num=[1,2,3,4];
+
